2f90007782d1a4e1bf4cb04a4e1832e7bf9ced4d
Kilwostar <35807163+Kilwostar@users.noreply.github.com>
2022-10-20 11:57:39 +0300

    wet braking distance calculation

--- untitled/src/BrakingDistanceCalculator.java
+++ untitled/src/BrakingDistanceCalculator.java
@@ -1,29 +1,53 @@
 import java.util.Scanner;
 
-public class brakingDistance {
-    int brakingFactor = 1;
-    double s;
+public class BrakingDistanceCalculator {
+    double S;
 
     public double brakingDistance(int Speed, int Condotion, int TypeOfRoad) {
         if (Condotion == 1) {
             switch (TypeOfRoad) {
                 case 1:
-                    s = (brakingFactor * (Speed * Speed)) / (254 * 0.75);
+                    S = (1 * (Speed * Speed)) / (254 * 0.75);
                     break;
                 case 2:
-                    s = (brakingFactor * (Speed * Speed)) / (254 * 0.75);
+                    S = (1 * (Speed * Speed)) / (254 * 0.75);
                     break;
                 case 3:
-                    s = (brakingFactor * (Speed * Speed)) / (254 * 0.65);
+                    S = (1 * (Speed * Speed)) / (254 * 0.65);
                     break;
                 case 4:
-                    s = (brakingFactor * (Speed * Speed)) / (254 * 0.55);
+                    S = (1 * (Speed * Speed)) / (254 * 0.55);
                     break;
                 case 5:
-                    s = (brakingFactor * (Speed * Speed)) / (254 * 0.5);
+                    S = (1 * (Speed * Speed)) / (254 * 0.5);
                     break;
             }
         }
-        return 0;
+        if(Condotion == 2){
+            switch (TypeOfRoad){
+                case 1:
+                    S = (1 * (Speed * Speed)) / (254 * 0.50);
+                    break;
+                case 2:
+                    S = (1 * (Speed * Speed)) / (254 * 0.60);
+                    break;
+                case 3:
+                    S = (1 * (Speed * Speed)) / (254 * 0.45);
+                    break;
+                case 4:
+                    S = (1 * (Speed * Speed)) / (254 * 0.45);
+                    break;
+                case 5:
+                    S = (1 * (Speed * Speed)) / (254 * 0.3);
+                    break;
+                case 6:
+                    S = (1 * (Speed * Speed)) / (254 * 0.2);
+                    break;
+                case 7:
+                    S = (1 * (Speed * Speed)) / (254 * 0.2);
+                    break;
+            }
+        }
+        return S;
     }
 }
